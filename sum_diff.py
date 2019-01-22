4c6bfacb630f96653b6680044143fa46903484c0
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-01-22 23:17:34 +1100

    Make sum_diff stack deeper

--- sum_diff.py
+++ sum_diff.py
@@ -1,12 +1,31 @@
+from numbers import Number
+
 
 def add(arg1, arg2):
     return arg1 + arg2
 
+
 def sub(arg1, arg2):
+    assert isinstance(arg1, Number)
+    assert isinstance(arg2, Number)
+    result = sub_pre_checked(arg1, arg2)
+    return result
+
+
+def sub_pre_checked(arg1, arg2):
     return arg1 - arg2
 
+
 def div(arg1, arg2):
+    assert isinstance(arg1, Number)
+    assert isinstance(arg2, Number)
+    result = div_pre_checked(arg1, arg2)
+    return result
+
+
+def div_pre_checked(arg1, arg2):
     return arg1 / arg2
+
 
 def sum_over_difference(arg1, arg2):
     """Compute sum of arguments over difference of arguments."""
