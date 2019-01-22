62a6021917a8d558a7fead85f22009877a817efd
Juan Nunez-Iglesias <juan.nunez-iglesias@monash.edu>
2019-01-23 00:13:44 +1100

    Tweak niblack exercise

--- exercises/test_niblack.py
+++ exercises/test_niblack.py
@@ -5,6 +5,20 @@
 import numpy as np
 from niblack import threshold_niblack
 
-image = np.full((4, 4), 0.03082192 + 2.19178082e-09)
 
-print(threshold_niblack(image))
+image1 = np.zeros((5, 5))
+image1[1:3, 1:3] = 0.5
+image1[2, 2] = 0.7
+
+print('\n\nimage1 image, threshold, thresholded')
+print(image1)
+print(threshold_niblack(image1, 3))
+print(image1 > threshold_niblack(image1, 3))
+
+
+image2 = np.full((4, 4), 0.03082192 + 2.19178082e-09)
+
+print('\n\nimage2 image, threshold, thresholded')
+print(image2)
+print(threshold_niblack(image2))
+print(image2 > threshold_niblack(image2))
